df333f65b7586487086f4f6f258913316408314d
08 <139735318+asdf080@users.noreply.github.com>
2023-12-18 13:48:19 +0900

    [level unrated] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,8 @@
+function solution(n, slicer, num_list) {
+    if(n == 1) return num_list.slice(0, slicer[1]+1)
+    if(n == 2) return num_list.slice(slicer[0])
+    if(n == 3) return num_list.slice(slicer[0], slicer[1]+1)
+    if(n == 4) {
+       return num_list.slice(slicer[0], slicer[1]+1).filter((_,i) => !(i%slicer[2]))
+    }
+  }
