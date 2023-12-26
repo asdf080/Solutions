b1467957efde82216f2fe7defcc5930bf421debf
08 <139735318+asdf080@users.noreply.github.com>
2023-12-26 14:24:48 +0900

    [level 1] Title: 직사각형 별찍기, Time: 101.64 ms, Memory: 39.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,10 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    for(let i=0;i<n[1];i++){
+        for(let j=0;j<n[0];j++){
+            process.stdout.write("*")
+        }
+        console.log('')
+    }
+});
