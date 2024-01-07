bd041354563a5ad62e9b6e07b2f571d4333e37bb
08 <139735318+asdf080@users.noreply.github.com>
2024-01-07 21:21:20 +0900

    [Bronze V] Title: 구구단, Time: 120 ms, Memory: 9316 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,12 @@
+const input = Number(require("fs").readFileSync("/dev/stdin", "utf8"))
+console.log(
+`${input} * 1 = ${input*1}\n`+
+`${input} * 2 = ${input*2}\n`+
+`${input} * 3 = ${input*3}\n`+
+`${input} * 4 = ${input*4}\n`+
+`${input} * 5 = ${input*5}\n`+
+`${input} * 6 = ${input*6}\n`+
+`${input} * 7 = ${input*7}\n`+
+`${input} * 8 = ${input*8}\n`+
+`${input} * 9 = ${input*9}`
+)
