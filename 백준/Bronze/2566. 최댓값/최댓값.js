53763e4e10b14ab42de02859a9135f588d558d44
08 <139735318+asdf080@users.noreply.github.com>
2024-01-23 13:56:18 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 9452 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.js
+++ 백준/Bronze/2566. 최댓값/최댓값.js
@@ -1,5 +1,5 @@
 const input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n").map((x) => x.trim().split(" "));
-let n = x = y = 0;
+let n = 0;
 let arr = [];
 for (let i = 0; i < 9; i++) {
   n = Math.max(n, ...input[i].map(Number));
@@ -7,13 +7,11 @@ for (let i = 0; i < 9; i++) {
 for (let j = 0; j < 9; j++) {
   for (k = 0; k < 9; k++) {
     if (input[j][k] == n) {
-      x = j;
-      y = k;
-      break;
+      arr.push(j);
+      arr.push(k);
     }
   }
-  if (arr.length == 2) break;
 }
 
 console.log(n);
-console.log(`${x + 1} ${y + 1}`);
+console.log(...arr.map((a) => a + 1));
