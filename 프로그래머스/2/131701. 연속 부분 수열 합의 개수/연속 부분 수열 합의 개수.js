926cf654ee8a9292df29fac8e461dd381becc2e3
08 <139735318+asdf080@users.noreply.github.com>
2024-04-17 11:34:29 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 137.72 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -4,14 +4,7 @@ function solution(elements) {
     for (let i = 1; i <= elements.length; i++) {
       let sum = 0;
       for (let j = 0; j < elements.length; j++) {
-        if (j === 0) {
-          for (let k = 0; k < i; k++) {
-            sum += elements[k]
-          }
-        } else {
-          sum -= elements[j - 1];
-          sum += elements[(j + i - 1) % elements.length]
-        }
+        sum += elements[(j + i - 1) % elements.length]
         set.add(sum);
       }
     }
