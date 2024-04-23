dfc1533326d95da3cb54e3122e27946e13437575
08 <139735318+asdf080@users.noreply.github.com>
2024-04-23 14:14:26 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 188 ms, Memory: 10072 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.js
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.js
@@ -1,4 +1,4 @@
-const [a, b, c, d, e, f] = require("fs").readFileSync("/dev/stdin", "utf8").trim().split(" ").map(Number);
+const [a, b, c, d, e, f] = `${require("fs").readFileSync(0)}`.split(" ").map(Number);
 
 outer: for (let i = -999; i < 1000; i++) {
   for (let j = -999; j < 1000; j++) {
