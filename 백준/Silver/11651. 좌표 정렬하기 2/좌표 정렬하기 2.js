36a897570df41e93e42f5a40c6f8e99622498920
08 <139735318+asdf080@users.noreply.github.com>
2024-05-28 14:01:10 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2556 ms, Memory: 43752 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,8 @@
+const [, ...input] = require("fs").readFileSync(0, "utf8").trim().split("\n")
+input.sort((a,b)=> {
+  const [a1, a2] = a.split(" ").map(Number)
+  const [b1, b2] = b.split(" ").map(Number)
+  if(a2 === b2) return a1 - b1
+  return a2 - b2
+})
+console.log(input.join("\n"));
