47f3a66f064e18d833f182324f5edcb126a23f7f
08 <139735318+asdf080@users.noreply.github.com>
2024-05-28 14:23:46 +0900

    [Silver V] Title: 나이순 정렬, Time: 1200 ms, Memory: 32340 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -1,8 +1,2 @@
 const [, ...input] = require("fs").readFileSync(0, "utf8").trim().split("\n")
-let arr = input
-arr.sort((a,b)=> {
-  let [a1, a2] = a.split(" ")
-  let [b1, b2] = b.split(" ")
-  return a1 - b1
-})
-console.log(arr.join("\n"));
+console.log(input.sort((a,b)=>  a.split(" ")[0] - b.split(" ")[0]).join("\n"));
