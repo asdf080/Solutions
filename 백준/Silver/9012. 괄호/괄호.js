701de5dd66c83655122a56a2c068586348d5c625
08 <139735318+asdf080@users.noreply.github.com>
2024-06-06 15:40:11 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,8 +1,11 @@
 const [a, ...input] = require("fs").readFileSync(0, "utf8").trim().split("\n");
 
-for (let i = 0; i < input.length; i++) {
-  while (input[i].includes("()")) {
-    input[i] = input[i].replaceAll("()", "");
-  }
-}
-input.forEach((ip) => (ip === "" ? console.log("YES") : console.log("NO")));
+console.log(
+  input.map((_, idx) => {
+    while (input[idx].includes("()")) {
+      input[idx] = input[idx].replaceAll("()", "");
+    }
+    if (input[idx] === "") return "YES";
+    else return "NO";
+  }).join("\n")
+)
