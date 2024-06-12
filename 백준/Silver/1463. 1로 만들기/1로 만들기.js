93e4bc3162585f6a46ebf989d27009630125a6ef
08 <139735318+asdf080@users.noreply.github.com>
2024-06-12 17:32:57 +0900

    [Silver III] Title: 1로 만들기, Time: 224 ms, Memory: 43716 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,8 +1,8 @@
 const input = +require("fs").readFileSync(0, "utf8").trim();
 
-let arr = [0, 0, 1, 1];
+let arr = [0, 0, 1];
 
-for (let i = 4; i <= input; i++) {
+for (let i = 3; i <= input; i++) {
   let a = arr[i - 1] + 1;
   let b = i % 3 === 0 ? arr[i / 3] + 1 : 100000;
   let c = i % 2 === 0 ? arr[i / 2] + 1 : 100000;
