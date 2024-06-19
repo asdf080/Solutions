66514e9acf2f5d6a991119a29d686aa172946e10
08 <139735318+asdf080@users.noreply.github.com>
2024-06-19 16:32:24 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 9432 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,22 +1,18 @@
 let [a, b, ...inp] = require("fs").readFileSync(0, "utf8").trim().split("\n"),
-  graph = Array.from({ length: ++a }, () => []),
-  visit = Array(++a).fill(0);
+  graph = Array.from({ length: ++a }, () => []);
 inp = inp.map((ip) => ip.split(" ").map(Number));
-visit[1] = 1;
 
 for (let i = 0; i < b; i++) {
   graph[inp[i][0]].push(inp[i][1]);
   graph[inp[i][1]].push(inp[i][0]);
 }
 
-function dfs(pa) {
-  for (let a of graph[pa]) {
-    if (visit[a] === 0) {
-      visit[a] = 1;
-      dfs(a);
-    }
+function dfs(start, visit = new Set()) {
+  visit.add(start);
+  for (let a of graph[start]) {
+    if (!visit.has(a)) dfs(a, visit);
   }
+  return visit;
 }
 
-dfs(1);
-console.log(visit.filter((v) => v).length - 1);
+console.log(dfs(1).size - 1);
