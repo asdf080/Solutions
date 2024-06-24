50ebdeced4faf9de28e8073d0c0d341029a16eac
08 <139735318+asdf080@users.noreply.github.com>
2024-06-24 22:41:42 +0900

    [Bronze I] Title: 약수, Time: 140 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.js
+++ 백준/Bronze/1037. 약수/약수.js
@@ -1,3 +1,2 @@
-let [a, ...inp] = require("fs").readFileSync(0, "utf8").trim().split("\n");
-inp = inp[0].split(" ").map(Number);
+let inp = require("fs").readFileSync(0, "utf8").trim().split("\n")[1].split(" ");
 console.log(Math.min(...inp) * Math.max(...inp));
