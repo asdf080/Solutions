8e28b4246dee0daf0582c63fac6ad41942698cc0
asdf080 <139735318+asdf080@users.noreply.github.com>
2025-04-27 18:01:37 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.98 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,16 +1,13 @@
 function solution(s) {
-    if(s == 1) return [0,0]
-    let num1 = 0, num2 = 0;
-    while(s != 1) {
-        let arr = s.split('')
-        for(let i = 0;i<arr.length;i++){
-          if(arr[i] == '0'){
-            arr[i] = '';
-            num2++;
-          }
-        }
-        s = arr.join('').length.toString(2)
-        num1++
-        if(s == 1) return [num1, num2]
+    let num = 0;
+    let zero = 0;
+    while(s!=="1"){
+        let leng1 = s.length
+        s = s.replaceAll("0", "")
+        let leng2 = s.length
+        zero += leng1 - leng2
+        s = leng2.toString(2)
+        num++
     }
+    return [num, zero];
 }
